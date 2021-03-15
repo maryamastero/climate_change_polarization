ba429446ee00d022cefb381f9b7854acd7534d50
Maryam Kiashemshaki <maryam.kiashemshaki@aalto.fi>
2021-03-15 21:03:11 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+Fill these variables with your own. Don't push to the remote repository!
+To stop git from tracking this file, use the command:
+git update-index --assume-unchanged notebooks/config.py
+"""
+
+consumer_key = ''
+consumer_secret = ''
+data_filepath = ''
